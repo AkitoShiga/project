c2302fd0560c57ce14201876fff5417705c6b77c
akitoshi.shiga <akitoshi.shiga@insight-inc.biz>
2021-01-02 21:01:34 +0900

    login register実装できた

--- laravel-vue-app/app/Models/User.php
+++ laravel-vue-app/app/Models/User.php
@@ -5,12 +5,12 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HashApiTokens;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HashApiTokens,Notifiable;
-    //use HasFactory, Notifiable;
+    use HasApiTokens;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
